fbc5a5f39ae53ddb85282e70ac1633755a8a7832
Tufan Calisir <tufan.calisir@project-joker.com>
2023-08-24 11:12:19 +0200

    feat: og, sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://tufancalisir.de/',
   integrations: [tailwind({
     applyBaseStyles: false
   }), NetlifyCMS({
